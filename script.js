90e6236a8b45d56998224b7263b5744c44a870ae
Sylvester009 <sasy260902@gmail.com>
2024-05-22 23:53:19 +0100

    intro-component

---
+++ script.js
@@ -0,0 +1,64 @@
+const button = document.getElementById("button");
+const errMsg = document.getElementsByClassName("err-msg");
+const labels = document.getElementsByTagName("label");
+const errImg = document.getElementsByClassName("err-img");
+
+const firstName = document.getElementById("f-name");
+const lastName = document.getElementById("l-name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+button.addEventListener("click", function (event) {
+  // Prevent the default button click behavior
+  event.preventDefault();
+
+  // Get the inputs value
+  var firstNameValue = firstName.value;
+  var lastNameValue = lastName.value;
+  var passwordValue = password.value;
+  var emailValue = email.value;
+
+  // Regular expression for inputs validation
+  var firstNameRegex = /^[a-zA-Z]+$/;
+  var lastNameRegex = /^[a-zA-Z]+$/;
+  var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Check if the inputs are valid
+  const inputsValue = [firstNameValue, lastNameValue, emailValue, passwordValue];
+  const inputsRegex = [firstNameRegex, lastNameRegex, emailRegex, passwordRegex];
+
+  for (let i = 0; i < inputsRegex.length; i++) {
+    if (!inputsRegex[i].test(inputsValue[i])) {
+      errMsg[i].textContent = "Input is incorrect/empty";
+      labels[i].style.borderColor = "hsl(0, 93%, 68%)";
+      errImg[i].style.display = "block";
+      return;
+    } else {
+      errMsg[i].textContent = "valid Input"; // Clear the error message if input is valid
+      errMsg[i].style.color = "green";
+      errImg[i].style.display = "none"; // Hide the error image if input is valid
+      labels[i].style.borderColor = ""; // Reset label border color if input is valid
+      button.value = "Form Submitted";
+    }
+  }
+});
+
+
+/* Check if the inputs are valid
+  if (!emailRegex.test(emailValue) || !firstNameRegex.test(firstNameValue)) {
+    errMsg.textContent = "Invalid Input";
+    label.style.color = "hsl(354, 100%, 66%)";
+    form.style.borderColor = "hsl(0, 93%, 68%)";
+    error.style.display = "block";
+    return;
+  } else {
+    label.textContent = "Thank you! Your email address has been added";
+    label.style.color = "hsl(0, 36%, 70%)";
+    form.style.borderColor = "hsl(0, 36%, 70%)";
+    error.style.display = "none";
+
+    // Reset the email input value
+    email.value = "";
+    return;
+  }*/
